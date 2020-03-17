38755580cd74488ee1f3f5ae5b8bd441b5ad9ffc
Guillaume Maze <maze.guillaume@gmail.com>
2020-03-17 18:55:07 +0100

    Update __init__.py

--- argopy/__init__.py
+++ argopy/__init__.py
@@ -1,4 +1,10 @@
-__version__ = "0.1"
+try:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution("argopy").version
+except Exception:
+    # Local copy, not installed with setuptools, or setuptools is not available.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
 
 # Import facades:
 from .fetchers import ArgoDataFetcher as DataFetcher
