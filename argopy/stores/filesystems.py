42c7bf996b2d0bd77fff63e337fc3ac7cd5eb24e
Guillaume Maze <gmaze@ifremer.fr>
2021-10-07 10:59:38 +0200

    Fix CWE-377 security alert
    fix code scanning alert: https://github.com/euroargodev/argopy/security/code-scanning/1?query=ref%3Arefs%2Fheads%2Fmaster

--- argopy/stores/filesystems.py
+++ argopy/stores/filesystems.py
@@ -136,7 +136,6 @@ class argo_store_proto(ABC):
     def _clear_cache_item(self, uri):
         """ Open fsspec cache registry (pickle file) and remove entry for uri
 
-            todo: Replace by the pop_from_cache fsspec method introduced in 0.7.4 in another PR
         """
         # See the "save_cache()" method in:
         # https://filesystem-spec.readthedocs.io/en/latest/_modules/fsspec/implementations/cached.html#WholeFileCacheFileSystem
@@ -153,17 +152,15 @@ class argo_store_proto(ABC):
                 cache[k] = v.copy()
             else:
                 os.remove(os.path.join(self.fs.storage[-1], v['fn']))
-        fn2 = tempfile.mktemp()
-        with open(fn2, "wb") as f:
+        with tempfile.NamedTemporaryFile(mode="wb", delete=False) as f:
             pickle.dump(cache, f)
-        shutil.move(fn2, fn)
+        shutil.move(f.name, fn)
 
     def clear_cache(self):
         """ Remove cache files and entry from uri open with this store instance """
         if self.cache:
             for uri in self.cache_registry:
                 self._clear_cache_item(uri)
-                # self.fs.pop_from_cache(uri)
 
     @abstractmethod
     def open_dataset(self, *args, **kwargs):
@@ -541,12 +538,9 @@ class httpstore(argo_store_proto):
 
         """
         log.debug("Reading csv: %s" % url)
-        try:
-            with self.open(url) as of:
-                df = pd.read_csv(of, **kwargs)
-            return df
-        except requests.HTTPError as e:
-            self._verbose_exceptions(e)
+        with self.open(url) as of:
+            df = pd.read_csv(of, **kwargs)
+        return df
 
     def open_json(self, url, **kwargs):
         """ Return a json from an url, or verbose errors
@@ -567,8 +561,6 @@ class httpstore(argo_store_proto):
             return js
         except json.JSONDecodeError:
             raise
-        except requests.HTTPError as e:
-            self._verbose_exceptions(e)
 
     def _mfprocessor_json(self, url, preprocess=None, *args, **kwargs):
         # Load data
