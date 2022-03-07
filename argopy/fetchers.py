033920cbce7a970591205a4db7c6f65aa53564dc
Guillaume Maze <gmaze@ifremer.fr>
2022-03-07 10:46:35 +0100

    Fix access-member-before-definition (E0203)

--- argopy/fetchers.py
+++ argopy/fetchers.py
@@ -139,6 +139,7 @@ class ArgoDataFetcher:
 
         # More init:
         self._loaded = False
+        self._request = ''
 
         # Dev warnings
         # Todo Clean-up before each release
@@ -659,12 +660,12 @@ class ArgoIndexFetcher:
         self.postproccessor = self.__empty_processor
         self._AccessPoint = None
 
-
         # Init data structure holders:
         self._index = None
 
         # More init:
         self._loaded = False
+        self._request = ''
 
     def __repr__(self):
         if self.fetcher:
