2718c18bb3ff9d3e9fe6ce2b3aecf6ed324c8604
Guillaume Maze <gmaze@ifremer.fr>
2022-03-07 09:36:52 +0100

    Try to fix critical issues raised by codacy.com

---
+++ argopy/data_fetchers/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package contains implementations for data and index fetchers for specific data sources.
+These fetchers are meant to be used and discovered automatically by the facades (in fetchers.py)
+and by utilities functions list_available_data_src() and list_available_index_src()
+"""
