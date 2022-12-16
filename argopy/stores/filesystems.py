05e045574c1fcaf2e523f40883931f09aae5f683
Guillaume Maze <gmaze@ifremer.fr>
2022-12-16 14:43:17 +0100

    Minor changes to doc

--- argopy/stores/filesystems.py
+++ argopy/stores/filesystems.py
@@ -823,9 +823,9 @@ class memorystore(filestore):
             Special handling for memory store
 
             The fsspec.exists() will return False even if the path is in cache.
-            Here we bypass this in order to return True if the path is in cache
-            This assumes that the goal of fs.exists is to determine if we can load the path or not
-            If the path is in cache, it can be loaded
+            Here we bypass this in order to return True if the path is in cache.
+            This assumes that the goal of fs.exists is to determine if we can load the path or not.
+            If the path is in cache, it can be loaded.
         """
         guess = self.fs.exists(path, *args)
         if not guess:
