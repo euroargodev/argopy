277a3368c9434a444515440b71853073f9cf15af
RaphaelBajon <raphael.bajon@protonmail.com>
2023-02-13 15:50:26 +0100

    Uncomment variable list verification

--- argopy/xarray.py
+++ argopy/xarray.py
@@ -247,7 +247,7 @@ class ArgoAccessor:
             "VERTICAL_SAMPLING_SCHEME",
             "FLOAT_SERIAL_NO",
         ]
-        print(list_str)
+        #print(list_str)
         list_int = [
             "PLATFORM_NUMBER",
             "WMO_INST_TYPE",
