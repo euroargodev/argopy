3aa1f9a7d0be33cbcf93da3543c3d13ca80b31c2
Guillaume Maze <gmaze@ifremer.fr>
2023-03-08 14:41:49 +0100

    Implement mocked FTP server in CI tests
    - Requires pytest-localftpserver
    - In conftests set up a mocked ftp server with the content of the tutorial GDAC data
    - In GDAC data fetcher tests, uses the new mocked ftp server and no longer tests the Ifremer FTP

--- argopy/tests/conftest.py
+++ argopy/tests/conftest.py
@@ -1,4 +1,40 @@
 import sys
 import os
+import logging
+from argopy import tutorial
+import pytest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
+log = logging.getLogger("argopy.tests.conftests")
+
+def pytest_sessionstart(session):
+    log.debug("Starting tests session")
+    log.debug("Initial session state: %s" % session)
+    pass
+
+@pytest.fixture(scope="module", autouse=True)
+def mocked_ftpserver(ftpserver):
+    os.environ['FTP_USER'] = 'janedow'
+    os.environ['FTP_PASS'] = 'please'
+    os.environ['FTP_HOME'] = 'test_folder'
+    os.environ['FTP_PORT'] = '31175'
+
+    # Set up the ftp server with the tutorial repo GDAC content:
+    ftproot, flist = tutorial.open_dataset('localftp')
+    for f in flist:
+        ftpserver.put_files({"src": f,
+                             "dest": f.replace(ftproot, ".")},
+                            style="url", anon=True)
+
+    #
+    ftp_login_data = ftpserver.get_login_data()
+    MOCKFTP = ftpserver.get_login_data(style="url", anon=True)
+    pytest.MOCKFTP = MOCKFTP
+    log.debug("Mocked GDAC ftp server located at: %s" % MOCKFTP)
+
+    yield ftpserver
+
+def pytest_sessionfinish(session, exitstatus):
+    log.debug("Ending tests session")
+    log.debug("Final session state: %s" % session)
+    pass
