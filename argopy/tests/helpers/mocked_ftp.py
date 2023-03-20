ab81c91ac305fea13a68ed089a61587c3d07805c
Guillaume Maze <gmaze@ifremer.fr>
2023-03-20 11:36:45 +0100

    Update mocked_ftp.py

--- argopy/tests/helpers/mocked_ftp.py
+++ argopy/tests/helpers/mocked_ftp.py
@@ -29,7 +29,7 @@ def mocked_ftpserver(ftpserver):
     os.environ['FTP_PORT'] = str(ftp_login_data['port'])
     MOCKFTP = ftpserver.get_login_data(style="url", anon=True)
     pytest.MOCKFTP = MOCKFTP
-    log.info("Mocked GDAC ftp server ready (%s)" % MOCKFTP)
+    log.info("Mocked GDAC ftp server ready at %s with %i files" % (MOCKFTP, len(flist)))
 
     yield ftpserver
 
