44025d2e49bc3b6472356883ecd95e2cd86ecdbc
Guillaume Maze <gmaze@ifremer.fr>
2023-09-07 08:30:35 +0200

    Update __init__.py

--- argopy/related/__init__.py
+++ argopy/related/__init__.py
@@ -2,7 +2,7 @@ from .topography import TopoFetcher
 from .ocean_ops_deployments import OceanOPSDeployments
 from .reference_tables import ArgoNVSReferenceTables
 from .argo_documentation import ArgoDocs
-from .gdac_snapshot import ArgoDOI
+from .doi_snapshot import ArgoDOI
 
 
 #
