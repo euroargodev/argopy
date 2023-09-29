b8e377a680d948ec7d28c315b1225a5f010fff37
Guillaume Maze <gmaze@ifremer.fr>
2023-09-29 10:21:23 +0200

    fix discrete_coloring import

--- argopy/plot/__init__.py
+++ argopy/plot/__init__.py
@@ -4,7 +4,7 @@ Plot Submodule docstring !
 from .plot import plot_trajectory, bar_plot, open_sat_altim_report, scatter_map, scatter_plot
 from .argo_colors import ArgoColors
 from .dashboards import open_dashboard as dashboard
-from .utils import discrete_coloring, latlongrid
+from .utils import latlongrid
 
 
 __all__ = (
@@ -21,5 +21,4 @@ __all__ = (
 
     # Utils:
     "latlongrid",
-    "discrete_coloring",
 )
