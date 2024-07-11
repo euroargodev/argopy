8af82c7ed47b67f744b36fef2adb17f3fe0fd97e
Guillaume Maze <gmaze@ifremer.fr>
2024-07-11 15:10:40 +0200

    Update gdacftp_data.py

--- argopy/data_fetchers/gdacftp_data.py
+++ argopy/data_fetchers/gdacftp_data.py
@@ -448,7 +448,7 @@ class Fetch_wmo(FTPArgoDataFetcher):
         self.WMO = WMO
         self.CYC = CYC
         # self.N_FILES = len(self.uri)  # Trigger search in the index, should we do this at instantiation or later ???
-        self.N_FILES = np.NaN
+        self.N_FILES = np.nan
         self._nrows = None
         if "MAX_FILES" in kwargs:
             self._nrows = kwargs["MAX_FILES"]
