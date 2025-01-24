c299bb6671407eba29ebeacd9da8028c21faed91
Guillaume Maze <gmaze@ifremer.fr>
2025-01-24 13:37:27 +0100

    Update format.py

--- argopy/utils/format.py
+++ argopy/utils/format.py
@@ -137,6 +137,13 @@ def argo_split_path(this_path):  # noqa C901
     print("output:", output)
 
     try:
+        output["origin"] = sep.join(path_parts[0:path_parts.index('dac')])
+        print(output["origin"])
+        output["origin"] = sep if output["origin"] == "" else output['origin']
+        print(output["origin"])
+
+        output["path"] = sep.join(path_parts[path_parts.index('dac'):])
+
         if path_parts[-1] == "profiles":
             output["type"] = "Mono-cycle profile file"
             output["wmo"] = path_parts[-2]
@@ -220,22 +227,22 @@ def argo_split_path(this_path):  # noqa C901
 
     # Adjust origin and path for local files:
     # This ensures that output['path'] is agnostic to users and can be reused on any gdac compliant architecture
-    parts = path.split(sep)
-    i, stop = len(parts) - 1, False
-    while not stop:
-        if (
-            parts[i] == "profiles"
-            or parts[i] == output["wmo"]
-            or parts[i] == output["dac"]
-            or parts[i] == "dac"
-        ):
-            i = i - 1
-            if i < 0:
-                stop = True
-        else:
-            stop = True
-    output["origin"] = sep.join(parts[0 : i + 1])
-    output["path"] = output["path"].replace(output["origin"], "")
+    # parts = path.split(sep)
+    # i, stop = len(parts) - 1, False
+    # while not stop:
+    #     if (
+    #         parts[i] == "profiles"
+    #         or parts[i] == output["wmo"]
+    #         or parts[i] == output["dac"]
+    #         or parts[i] == "dac"
+    #     ):
+    #         i = i - 1
+    #         if i < 0:
+    #             stop = True
+    #     else:
+    #         stop = True
+    # output["origin"] = sep.join(parts[0 : i + 1])
+    # output["path"] = output["path"].replace(output["origin"], "")
 
     return dict(sorted(output.items()))
 
