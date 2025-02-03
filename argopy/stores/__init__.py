64ec945ec95aa29ef69e3c2efe4cd9e357c94851
Guillaume Maze <gmaze@ifremer.fr>
2025-02-03 10:59:06 +0100

    Update __init__.py
    missing new ArgoFloat

--- argopy/stores/__init__.py
+++ argopy/stores/__init__.py
@@ -5,12 +5,16 @@ from .implementations.http_erddap import httpstore_erddap
 from .implementations.ftp import ftpstore
 from .implementations.s3 import s3store
 from .implementations.gdac import gdacfs
+
 from .index.argo_index import ArgoIndex
+from .float.argo_float import ArgoFloat
+
 from .kerchunker import ArgoKerchunker
 
 from .filesystems import has_distributed, distributed  # noqa: F401
 from .spec import ArgoStoreProto  # noqa: F401
 from .implementations.http_erddap import httpstore_erddap_auth  # noqa: F401
+
 from .index.implementations.index_pyarrow import (
     indexstore as indexstore_pa,
 )  # noqa: F401
@@ -22,6 +26,7 @@ from .index.implementations.index_pandas import (
 __all__ = (
     # Classes:
     "ArgoIndex",
+    "ArgoFloat",
     "filestore",
     "httpstore",
     "httpstore_erddap",
