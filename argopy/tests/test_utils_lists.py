ee87cbc409832a60c8023f1d1f39bc130c0bf853
Guillaume Maze <gmaze@ifremer.fr>
2025-03-04 09:21:38 +0100

    [skip-ci]

--- argopy/tests/test_utils_lists.py
+++ argopy/tests/test_utils_lists.py
@@ -1,7 +1,15 @@
-# import pytest
+import pytest
 from argopy.utils.checkers import is_list_of_strings
-from argopy.utils.lists import list_multiprofile_file_variables
+from argopy.utils.lists import list_multiprofile_file_variables, shortcut2gdac
 
 
 def test_list_multiprofile_file_variables():
     assert is_list_of_strings(list_multiprofile_file_variables())
+
+shortcuts = {None: dict, 'ftp': str, 'https://data-argo.ifremer.fr': str}
+
+@pytest.mark.parametrize("short", shortcuts.items(),
+                         indirect=False,
+                         ids=["host=%s" % p for p in shortcuts.keys()])
+def test_shortcut2gdac(short):
+    assert isinstance(shortcut2gdac(short[0]), short[1])
