31268fa076d9d22d340ee80f83e578b456c4747a
Guillaume Maze <gmaze@ifremer.fr>
2025-03-06 09:49:22 +0100

    Refactor indexstore_pa and indexstore_pd accessory imports for debug
    - moved this up one level at the stores/index/__init__

---
+++ argopy/stores/index/__init__.py
@@ -0,0 +1,13 @@
+from .index.implementations.pyarrow.index import (
+    indexstore as indexstore_pa,
+)  # noqa: F401
+from .index.implementations.pandas.index import (
+    indexstore as indexstore_pd,
+)  # noqa: F401
+
+
+__all__ = (
+    # Classes:
+    "indexstore_pa",
+    "indexstore_pd",
+)
