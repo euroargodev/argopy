bb2dd9ebaf1604e2fe69ce9a64bbca717e7d3325
Guillaume Maze <gmaze@ifremer.fr>
2025-03-26 16:15:23 +0100

    renamed new argo extension

--- argopy/extensions/optical_modeling.py
+++ argopy/extensions/optical_modeling.py
@@ -1,3 +1,5 @@
+import xarray as xr
+
 from ..utils.optical_modeling import Z_euphotic
 from . import register_argo_accessor, ArgoAccessorExtension
 
@@ -9,18 +11,21 @@ class OpticalModeling(ArgoAccessorExtension):
     Examples
     --------
     .. code-block:: python
-        :caption: Example 1
+        :caption: Example
 
-        >>> from argopy import DataFetcher
-        >>> ds = Datafetcher(ds='bgc', mode='expert', params='DOWNWELLING_PAR').float(6901864).data
-        >>> dsp = ds.argo.point2profile()
-        >>> dsp.argo.optic.Zeu()
+        from argopy import DataFetcher
+        ds = Datafetcher(ds='bgc', mode='expert', params='DOWNWELLING_PAR').float(6901864).data
+        dsp = ds.argo.point2profile()
+
+        dsp.argo.optic.Zeu()
+        dsp.argo.optic.Zeu(axis='PRES')
+        dsp.argo.optic.Zeu(axis='PRES_ADJUSTED')
 
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def Zeu(self, par='DOWNWELLING_PAR', axis='PRES'):
+    def Zeu(self, par='DOWNWELLING_PAR', axis='PRES', max_surface=5., inplace=False):
         """Compute depth of the euphotic zone
 
         This is the depth at which the downwelling irradiance is reduced to 1% of its surface value.
@@ -29,9 +34,15 @@ class OpticalModeling(ArgoAccessorExtension):
 
         The downwelling irradiance is from the photosynthetically available radiation: PAR.
         """
-        da = self._argo.reduce_profile(Z_euphotic, par, axis)
+        # Computation:
+        da = self._argo.reduce_profile(Z_euphotic, params=[axis, par], max_surface=max_surface)
+
+        # Attributes
         da.name = 'Zeu'
         da.attrs = {'long_name': 'Depth of the euphotic zone',
                     'units': self._obj[axis].attrs.get('units', '?'),
                     'definition': 'Depth at which the downwelling irradiance is reduced to 1% of its surface value.'}
-        return da
+        if inplace:
+            self._obj['Zeu'] = da
+        else:
+            return da
