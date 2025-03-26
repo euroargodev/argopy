85aa9eab8918e2096e9646453920e8303f43feee
Guillaume Maze <gmaze@ifremer.fr>
2025-03-26 16:15:35 +0100

    Update __init__.py

--- argopy/extensions/__init__.py
+++ argopy/extensions/__init__.py
@@ -1,7 +1,7 @@
 from .utils import register_argo_accessor, ArgoAccessorExtension
 from .canyon_med import CanyonMED
 from .params_data_mode import ParamsDataMode
-from .bgc_optical_modeling import OpticalModeling
+from .optical_modeling import OpticalModeling
 
 #
 __all__ = (
