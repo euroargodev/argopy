129994240819a30a065c1521b171d35448ec464a
Guillaume Maze <gmaze@ifremer.fr>
2025-07-30 20:36:35 +0200

    Create plot.py

---
+++ argopy/stores/index/implementations/plot.py
@@ -0,0 +1,85 @@
+from typing import Any
+
+from ....plot import plot_trajectory, bar_plot
+from ..extensions import ArgoIndexPlotProto
+
+
+class ArgoIndexPlot(ArgoIndexPlotProto):
+    """Extension providing plot methods"""
+
+    def trajectory(self, index:bool=False, **kwargs) -> Any:
+        """Quick map of profile index trajectories
+
+        Parameters
+        ----------
+        **kwargs
+            All arguments are passed to :meth:`argopy.plot.plot_trajectory`.
+
+        Returns
+        -------
+        tuple
+            Output from :class:`argopy.plot.plot_trajectory`, typically:
+                - fig: :class:`matplotlib.figure.Figure`
+                - ax: :class:`matplotlib.axes.Axes`
+
+        Examples
+        --------
+        .. code-block:: python
+
+            from argopy import ArgoIndex
+
+            idx = ArgoIndex().query.wmo(WMO)
+            idx.plot.trajectory()
+        """
+        default_opts = {'style': 'white'}
+        N = len(self._obj.read_wmo(index=index))
+        if N == 1:
+            default_opts['hue'] = 'cyc'
+            default_opts['add_legend'] = False
+            default_opts['palette'] = 'Spectral_r'
+            default_opts['cbar'] = True
+        fig, ax = plot_trajectory(
+                self._obj.to_dataframe(index=index),
+                **{**default_opts, **kwargs}
+        )
+        ax.set_title(self.get_title(index))
+        return fig, ax
+
+    def bar(self, by: str='dac', index:bool=False, **kwargs) -> Any:
+        """Bar plot of one index property
+
+        Parameters
+        ----------
+        by: str, default='dac'
+            The index property to plot, one in 'date', 'latitude', 'longitude', 'ocean', 'profiler_code', 'institution_code', 'date_update', 'wmo', 'cyc', 'institution', 'dac', 'profiler'.
+
+        **kwargs
+            All other arguments are passed to :meth:`argopy.plot.bar_plot`.
+
+        Returns
+        -------
+        tuple
+            Output from :class:`argopy.plot.plot_trajectory`, typically:
+                - fig: :class:`matplotlib.figure.Figure`
+                - ax: :class:`matplotlib.axes.Axes`
+
+        Examples
+        --------
+        .. code-block:: python
+
+            from argopy import ArgoIndex
+
+            idx = ArgoIndex()
+            idx.plot.bar('institution')
+        """
+        if by not in ['date', 'latitude', 'longitude', 'ocean', 'profiler_code', 'institution_code', 'date_update', 'wmo', 'cyc', 'institution', 'dac', 'profiler']:
+            raise ValueError('Invalid value for "by", must be in "date", "latitude", "longitude", "ocean", "profiler_code"')
+        if 'by' in kwargs:
+            kwargs.pop('by')
+        fig, ax = bar_plot(
+                self._obj.to_dataframe(index=index),
+                by = by,
+                **kwargs
+        )
+        ax.set_title(self.get_title(index))
+        return fig, ax
