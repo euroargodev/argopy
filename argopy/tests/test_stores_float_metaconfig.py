3bbca00fdce1bf3bad11183c5e46a45281db4f6a
Guillaume Maze <gmaze@ifremer.fr>
2026-01-03 23:03:03 +0100

    Update test_stores_float_metaconfig.py
    - refactor tests to improve design

--- argopy/tests/test_stores_float_metaconfig.py
+++ argopy/tests/test_stores_float_metaconfig.py
@@ -2,6 +2,7 @@ import pytest
 import logging
 import numpy as np
 import pandas as pd
+import tempfile
 
 import argopy
 from argopy.stores import ArgoFloat
@@ -15,190 +16,127 @@ from argopy.stores.float.implementations.offline.meta_config import (
 from utils import (
     requires_gdac,
 )
+from mocked_http import mocked_httpserver, mocked_server_address
 
 log = logging.getLogger("argopy.tests.floatstore.config")
 argopy.clear_cache()
 
-skip_online = pytest.mark.skipif(0, reason="Skipped tests for online implementation")
 skip_offline = pytest.mark.skipif(0, reason="Skipped tests for offline implementation")
-
-"""
-Select a GDAC host to be use for config extension tests
-"""
-VALID_HOST = argopy.tutorial.open_dataset("gdac")[0]  # Use local files
-# 'http1': mocked_server_address,  # Use the mocked http server
-# 'http2': 'https://data-argo.ifremer.fr',
-# 'ftp': "MOCKFTP",  # keyword to use a fake/mocked ftp server (running on localhost)
+skip_online = pytest.mark.skipif(0, reason="Skipped tests for online implementation")
 
 """
 List WMO to be tested, one for each mission
 """
 VALID_WMO = [13857, 3902131]
 
+
+class FloatStore_Config_Proto:
+
+    def setup_class(self):
+        self.cachedir = tempfile.mkdtemp()
+
+    @pytest.mark.parametrize(
+        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
+    )
+    def test_missions(self, wmo, config, mocked_httpserver):
+        # log.debug(type(config._metadata))
+        assert isinstance(config.n_missions, int)
+        assert isinstance(config.missions, list)
+        assert all(isinstance(item, int) for item in config.missions)
+
+    @pytest.mark.parametrize(
+        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
+    )
+    def test_params(self, wmo, config, mocked_httpserver):
+        assert isinstance(config.n_params, int)
+        assert config.n_params == len(config)
+        assert all(isinstance(item, str) for item in config.parameters)
+
+    @pytest.mark.parametrize(
+        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
+    )
+    def test_cycles(self, wmo, config, mocked_httpserver):
+        assert isinstance(config.cycles, dict)
+        assert all(isinstance(key, int) for key in config.cycles.keys())
+        assert all(isinstance(val, int) for val in config.cycles.values())
+
+    @pytest.mark.parametrize(
+        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
+    )
+    def test_get(self, wmo, config, mocked_httpserver):
+        a_param = np.random.choice(config.parameters, 1)[0]
+        assert isinstance(config[a_param], list)
+        assert isinstance(config[a_param, 1:2], list)
+        assert isinstance(config[a_param, 1], int | float | str | bool)
+
+    @pytest.mark.parametrize(
+        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
+    )
+    def test_for_cycles(self, wmo, config, mocked_httpserver):
+        a_param = np.random.choice(config.parameters, 1)[0]
+        cyc = config.cycles[np.random.choice(config.missions, 1)[0]]
+        assert isinstance(
+            config.for_cycles(a_param, cycle_numbers=cyc), int | float | str | bool
+        )
+
+    @pytest.mark.parametrize(
+        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
+    )
+    def test_to_dataframe(self, wmo, config, mocked_httpserver):
+        n = (config.n_params, config.n_missions)
+
+        for missions in [
+            None,
+            np.random.choice(config.missions, 1)[0],
+            np.random.choice(config.missions, 2),
+        ]:
+            nm = n[1] if missions is None else (missions := np.unique(missions)).size
+            df = config.to_dataframe(missions=missions)
+            assert isinstance(df, pd.DataFrame)
+            assert df.shape == (n[0], 3 + nm)
+
+
 @skip_offline
 @requires_gdac
-class Test_FloatStore_Config_Offline:
-    config = config_off
+class Test_FloatStore_Config_Offline(FloatStore_Config_Proto):
+    af: dict[int, ArgoFloat] = {}
 
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_missions(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        log.debug(type(cp._metadata))
+    # Define a fixture for an offline ArgoFloat instance (local store)
+    @pytest.fixture
+    def argo_float(self, wmo):
+        if wmo not in self.af:
+            self.af[wmo] = ArgoFloat(
+                wmo,
+                host=argopy.tutorial.open_dataset("gdac")[0],
+                cache=True,
+                cachedir=self.cachedir,
+            )
+        return self.af[wmo]
 
-        assert isinstance(cp.n_missions, int)
-        assert isinstance(cp.missions, list)
-        assert all(isinstance(item, int) for item in cp.missions)
+    # Define a fixture for the config extension
+    @pytest.fixture
+    def config(self, argo_float):
+        return config_off(argo_float)
 
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_params(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-
-        assert isinstance(cp.n_params, int)
-        assert cp.n_params == len(cp)
-        assert all(isinstance(item, str) for item in cp.parameters)
-
-        assert isinstance(cp.cycles, dict)
-        assert all(isinstance(key, int) for key in cp.cycles.keys())
-        assert all(isinstance(val, int) for val in cp.cycles.values())
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_cycles(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-
-        assert isinstance(cp.cycles, dict)
-        assert all(isinstance(key, int) for key in cp.cycles.keys())
-        assert all(isinstance(val, int) for val in cp.cycles.values())
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_get(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        a_param = np.random.choice(cp.parameters, 1)[0]
-        assert isinstance(cp[a_param], list)
-        assert isinstance(cp[a_param, 1:2], list)
-        assert isinstance(cp[a_param, 1], int | float | str | bool)
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_for_cycles(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        a_param = np.random.choice(cp.parameters, 1)[0]
-        cyc = cp.cycles[np.random.choice(cp.missions, 1)[0]]
-        assert isinstance(
-            cp.for_cycles(a_param, cycle_numbers=cyc), int | float | str | bool
-        )
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_to_dataframe(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        n = (cp.n_params, cp.n_missions)
-
-        for missions in [
-            None,
-            np.random.choice(cp.missions, 1)[0],
-            np.random.choice(cp.missions, 2),
-        ]:
-            nm = n[1] if missions is None else (missions := np.unique(missions)).size
-            df = cp.to_dataframe(missions=missions)
-            assert isinstance(df, pd.DataFrame)
-            assert df.shape == (n[0], 3 + nm)
 
 @skip_online
-@requires_gdac
-class Test_FloatStore_Config_Online:
-    config = config_on
+class Test_FloatStore_Config_Online(FloatStore_Config_Proto):
+    af: dict[int, ArgoFloat] = {}
 
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_missions(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        log.debug(type(cp._metadata))
+    # Define a fixture for an online ArgoFloat instance (but using mocked http)
+    @pytest.fixture
+    def argo_float(self, wmo, mocked_httpserver):
+        if wmo not in self.af:
+            self.af[wmo] = ArgoFloat(
+                wmo,
+                host=mocked_server_address,
+                cache=True,
+                cachedir=self.cachedir,
+                eafleetmonitoring_server=mocked_server_address,
+            )
+        return self.af[wmo]
 
-        assert isinstance(cp.n_missions, int)
-        assert isinstance(cp.missions, list)
-        assert all(isinstance(item, int) for item in cp.missions)
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_params(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-
-        assert isinstance(cp.n_params, int)
-        assert cp.n_params == len(cp)
-        assert all(isinstance(item, str) for item in cp.parameters)
-
-        assert isinstance(cp.cycles, dict)
-        assert all(isinstance(key, int) for key in cp.cycles.keys())
-        assert all(isinstance(val, int) for val in cp.cycles.values())
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_cycles(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-
-        assert isinstance(cp.cycles, dict)
-        assert all(isinstance(key, int) for key in cp.cycles.keys())
-        assert all(isinstance(val, int) for val in cp.cycles.values())
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_get(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        a_param = np.random.choice(cp.parameters, 1)[0]
-        assert isinstance(cp[a_param], list)
-        assert isinstance(cp[a_param, 1:2], list)
-        assert isinstance(cp[a_param, 1], int | float | str | bool)
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_for_cycles(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        a_param = np.random.choice(cp.parameters, 1)[0]
-        cyc = cp.cycles[np.random.choice(cp.missions, 1)[0]]
-        assert isinstance(
-            cp.for_cycles(a_param, cycle_numbers=cyc), int | float | str | bool
-        )
-
-    @pytest.mark.parametrize(
-        "wmo", VALID_WMO, indirect=False, ids=[f"wmo={w}" for w in VALID_WMO]
-    )
-    def test_to_dataframe(self, wmo):
-        af = ArgoFloat(wmo, host=VALID_HOST, cache=True)
-        cp = self.config(af)
-        n = (cp.n_params, cp.n_missions)
-
-        for missions in [
-            None,
-            np.random.choice(cp.missions, 1)[0],
-            np.random.choice(cp.missions, 2),
-        ]:
-            nm = n[1] if missions is None else (missions := np.unique(missions)).size
-            df = cp.to_dataframe(missions=missions)
-            assert isinstance(df, pd.DataFrame)
-            assert df.shape == (n[0], 3 + nm)
+    # Define a fixture for the config extension
+    @pytest.fixture
+    def config(self, argo_float):
+        return config_on(argo_float)
