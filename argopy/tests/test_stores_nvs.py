747183ef8db6e453e23849386974a1e27d3af49e
Guillaume Maze <gmaze@ifremer.fr>
2026-01-25 17:39:39 +0100

    Add unit tests and test data for NVS store

---
+++ argopy/tests/test_stores_nvs.py
@@ -0,0 +1,163 @@
+import pytest
+import logging
+
+from argopy.stores.nvs.implementations.offline.nvs import NVS as NVS_offline
+from argopy.stores.nvs.implementations.online.nvs import NVS as NVS_online
+from argopy.stores.nvs.spec import NVSProto as NVS
+
+
+from mocked_http import mocked_httpserver, mocked_server_address
+
+log = logging.getLogger("argopy.tests.nvsstores")
+
+skip_offline = pytest.mark.skipif(0, reason="Skipped tests for offline implementation")
+skip_online = pytest.mark.skipif(0, reason="Skipped tests for online implementation")
+
+
+USE_MOCKED_SERVER = True
+
+RTID_TESTED = ['R27']
+CONCEPTID_TESTED = ['AANDERAA_OPTODE_3835']
+FMT_TESTED = ['json', 'xml', 'turtle']
+DATA_VALIDATOR = {
+        "json": lambda x: isinstance(x, dict),
+        "xml": lambda x: x.startswith('<?xml'),
+        "turtle": lambda x: x.startswith('PREFIX')}
+
+def is_offline_urivalid(url: str) -> bool:
+    """Use to validate an offline asset uri to be loaded with :class:`Asset`
+
+    This will depend on the :class:`Asset` implementation.
+    """
+    return isinstance(url, str) and url.startswith('vocabulary:offline:')
+
+def is_online_urivalid(url: str) -> bool:
+    """Use to validate an online asset uri to be loaded with :class:`Asset`
+
+    This will depend on the :class:`Asset` implementation.
+    """
+    return isinstance(url, str) and url.startswith('http') and 'current' in url
+
+@skip_offline
+class Test_NVS_Offline():
+    #############
+    # UTILITIES #
+    #############
+    @pytest.fixture
+    def nvs(self) -> NVS:
+        defaults_args = {}
+        return NVS_offline(**defaults_args)
+
+    #########
+    # TESTS #
+    #########
+    def test_init(self, nvs):
+        assert nvs.online == False
+
+    def test_uniqueinstance(self):
+        nvs1 : NVS = NVS_offline()
+        nvs2 : NVS = NVS_offline()
+        assert nvs1.uid == nvs2.uid
+
+    def test_readonlyinstance(self, nvs):
+        with pytest.raises(AttributeError):
+            nvs.nvs = 'dummy'
+
+    @pytest.mark.parametrize("rtid", RTID_TESTED, indirect=False, ids=[f"rtid='{x}'" for x in RTID_TESTED])
+    def test_vocabulary2uri(self, nvs, rtid):
+        assert is_offline_urivalid(nvs._vocabulary2uri(rtid))
+
+    @pytest.mark.parametrize("fmt", ['xml', 'turtle'], indirect=False, ids=[f"fmt='{x}'" for x in ['xml', 'turtle']])
+    @pytest.mark.parametrize("rtid", RTID_TESTED, indirect=False, ids=[f"rtid='{x}'" for x in RTID_TESTED])
+    def test_vocabulary2uri_invalidformat(self, nvs, rtid, fmt):
+        with pytest.raises(ValueError):
+            nvs._vocabulary2uri(rtid, fmt=fmt)
+
+    @pytest.mark.parametrize("fmt", ['json'], indirect=False, ids=[f"fmt='{x}'" for x in ['json']])
+    @pytest.mark.parametrize("rtid", ['R27', None], indirect=False, ids=[f"rtid='{x}'" for x in ['R27', None]])
+    @pytest.mark.parametrize("conceptid", ['AANDERAA_OPTODE_3835'], indirect=False, ids=[f"conceptid='{x}'" for x in ['AANDERAA_OPTODE_3835']])
+    def test_concept2uri(self, nvs, conceptid, rtid, fmt):
+        assert is_offline_urivalid(nvs._concept2uri(conceptid, rtid, fmt))
+
+    def test_concept2uri_errors(self, nvs):
+        conceptid = 'invalid'
+        with pytest.raises(ValueError):
+            nvs._concept2uri(conceptid)
+
+        conceptid = '1'
+        with pytest.raises(ValueError) as e:
+            nvs._concept2uri(conceptid)
+            assert "This Concept appears in more than one Vocabulary" in str(e)
+
+    @pytest.mark.parametrize("fmt", ['json'], indirect=False, ids=[f"fmt='{x}'" for x in ['json']])
+    @pytest.mark.parametrize("rtid", RTID_TESTED, indirect=False, ids=[f"rtid='{x}'" for x in RTID_TESTED])
+    def test_load_vocabulary(self, nvs, rtid, fmt):
+        assert DATA_VALIDATOR[fmt](nvs.load_vocabulary(rtid, fmt))
+
+    @pytest.mark.parametrize("fmt", ['json'], indirect=False, ids=[f"fmt='{x}'" for x in ['json']])
+    @pytest.mark.parametrize("rtid", RTID_TESTED, indirect=False, ids=[f"rtid='{x}'" for x in RTID_TESTED])
+    @pytest.mark.parametrize("conceptid", CONCEPTID_TESTED, indirect=False, ids=[f"conceptid='{x}'" for x in CONCEPTID_TESTED])
+    def test_load_concept(self, nvs, conceptid, rtid, fmt):
+        assert DATA_VALIDATOR[fmt](nvs.load_concept(conceptid, rtid, fmt))
+
+@skip_online
+class Test_NVS_Online():
+
+    #############
+    # UTILITIES #
+    #############
+    @pytest.fixture
+    def nvs(self):
+        defaults_args = {}
+        if USE_MOCKED_SERVER:
+            defaults_args["nvs"] = mocked_server_address
+
+        return NVS_online(**defaults_args)
+
+    #########
+    # TESTS #
+    #########
+    def test_init(self, nvs):
+        print(nvs)
+        assert nvs.online == True
+
+    def test_uniqueinstance(self, mocked_httpserver):
+        nvs1 : NVS = NVS_online(nvs=mocked_server_address)
+        nvs2 : NVS = NVS_online(nvs=mocked_server_address)
+        assert nvs1.uid == nvs2.uid
+
+    def test_readonlyinstance(self, nvs):
+        with pytest.raises(AttributeError):
+            nvs.nvs = 'dummy'
+
+    @pytest.mark.parametrize("fmt", FMT_TESTED, indirect=False, ids=[f"fmt='{x}'" for x in FMT_TESTED])
+    @pytest.mark.parametrize("rtid", RTID_TESTED, indirect=False, ids=[f"rtid='{x}'" for x in RTID_TESTED])
+    def test_vocabulary2uri(self, nvs, rtid, fmt):
+        assert is_online_urivalid(nvs._vocabulary2uri(rtid, fmt))
+
+    @pytest.mark.parametrize("fmt", FMT_TESTED, indirect=False, ids=[f"fmt='{x}'" for x in FMT_TESTED])
+    @pytest.mark.parametrize("rtid", ['R27', None], indirect=False, ids=[f"rtid='{x}'" for x in ['R27', None]])
+    @pytest.mark.parametrize("conceptid", ['AANDERAA_OPTODE_3835'], indirect=False, ids=[f"conceptid='{x}'" for x in ['AANDERAA_OPTODE_3835']])
+    def test_concept2uri(self, nvs, conceptid, rtid, fmt):
+        assert is_online_urivalid(nvs._concept2uri(conceptid, rtid, fmt))
+
+    def test_concept2uri_errors(self, nvs):
+        conceptid = 'invalid'
+        with pytest.raises(ValueError):
+            nvs._concept2uri(conceptid)
+
+        conceptid = '1'
+        with pytest.raises(ValueError) as e:
+            nvs._concept2uri(conceptid)
+            assert "This Concept appears in more than one Vocabulary" in str(e)
+
+    @pytest.mark.parametrize("fmt", FMT_TESTED, indirect=False, ids=[f"fmt='{x}'" for x in FMT_TESTED])
+    @pytest.mark.parametrize("rtid", RTID_TESTED, indirect=False, ids=[f"rtid='{x}'" for x in RTID_TESTED])
+    def test_load_vocabulary(self, nvs, rtid, fmt):
+        assert DATA_VALIDATOR[fmt](nvs.load_vocabulary(rtid, fmt))
+
+    @pytest.mark.parametrize("fmt", FMT_TESTED, indirect=False, ids=[f"fmt='{x}'" for x in FMT_TESTED])
+    @pytest.mark.parametrize("rtid", RTID_TESTED, indirect=False, ids=[f"rtid='{x}'" for x in RTID_TESTED])
+    @pytest.mark.parametrize("conceptid", CONCEPTID_TESTED, indirect=False, ids=[f"conceptid='{x}'" for x in CONCEPTID_TESTED])
+    def test_load_concept(self, nvs, conceptid, rtid, fmt):
+        assert DATA_VALIDATOR[fmt](nvs.load_concept(conceptid, rtid, fmt))
