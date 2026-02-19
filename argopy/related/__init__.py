ada8520735e10dfa6f2f7f778ff3caffb5d2595d
Guillaume Maze <gmaze@ifremer.fr>
2026-02-19 11:16:44 +0100

    Update __init__.py

--- argopy/related/__init__.py
+++ argopy/related/__init__.py
@@ -4,6 +4,7 @@ from .argo_documentation import ArgoDocs
 from .doi_snapshot import ArgoDOI
 from .euroargo_api import get_coriolis_profile_id, get_ea_profile_page
 from .utils import load_dict, mapp_dict  # Must come last to avoid circular import, I know, not good
+from argopy.reference import ArgoNVSReferenceTables  # For legacy import
 
 #
 __all__ = (
