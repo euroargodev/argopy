a397aba83ce4ece38926f19ac0205333ae7f7032
Charles Turner <52199577+charles-turner-1@users.noreply.github.com>
2026-09-10 20:55:36 +1000

    Update argopy/tests/test_utils_lists.py
    Signed-off-by: Charles Turner <52199577+charles-turner-1@users.noreply.github.com>

--- argopy/tests/test_utils_lists.py
+++ argopy/tests/test_utils_lists.py
@@ -22,14 +22,10 @@ def test_list_multiprofile_file_variables():
     assert is_list_of_strings(list_multiprofile_file_variables())
 
 
-shortcuts = {None: dict, "ftp": str, "https://data-argo.ifremer.fr": str}
+shortcuts = {None: dict, 'ftp': str, 'https://data-argo.ifremer.fr': str}
 
-
-@pytest.mark.parametrize(
-    "short",
-    shortcuts.items(),
-    indirect=False,
-    ids=["host=%s" % p for p in shortcuts.keys()],
-)
+@pytest.mark.parametrize("short", shortcuts.items(),
+                         indirect=False,
+                         ids=["host=%s" % p for p in shortcuts.keys()])
 def test_shortcut2gdac(short):
     assert isinstance(shortcut2gdac(short[0]), short[1])
